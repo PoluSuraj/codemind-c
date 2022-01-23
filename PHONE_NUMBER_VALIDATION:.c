9d8adffb616e90a1ed1e0480df6b46ca3140f79a
Suraj Kumar <97587580+PoluSuraj@users.noreply.github.com>
2022-01-23 19:35:38 +0530

    #include<stdio.h> #include<string.h> int main() {     int t;     char s[20];     // gets(s);     scanf("%[^ ]s",&s);     t=strlen(s);     if(t==10)     printf("Valid");     else     printf("Invalid");     return 0; }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    char s[20];
+    // gets(s);
+    scanf("%[^
+]s",&s);
+    t=strlen(s);
+    if(t==10)
+    printf("Valid");
+    else
+    printf("Invalid");
+    return 0;
+}
